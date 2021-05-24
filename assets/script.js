dc238994f39efb11a0e6201a58fb2a26d6224106
Joseph <joe@uhhuhohyeah.com>
2021-05-24 09:43:14 -0700

    intial commit with starting code

---
+++ assets/script.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// USER INPUT VARIABLES
+
+let passwordLength = prompt('How many characters in your password (8 - 20)?');
+let passwordUC = confirm ('Include uppercase letters');
+let passwordNUM = confirm ('Include numbers');
+let passwordSYM = confirm ('Include symbols');
+
+
+
+// Assignment code here
+
+// var numbers = arrayFromLowToHigh(48, 57);
+// var upperCase = arrayFromLowToHigh(65, 90);
+// var lowerCase = arrayFromLowToHigh(97, 122);
+// var specialChar = arrayFromLowToHigh(33, 47).concat(
+//   arrayFromLowToHigh(58, 64)).concat(
+//   arrayFromLowToHigh(91, 96)).concat(
+//   arrayFromLowToHigh(1213, 126))
+
+
+// var characterCountNumber = // from .document //
+// var includeNumberElement = // from .document //
+// var includeUppercaseElement = // from .document //
+// var includeLowercaseElement = // from .document //
+// var includeSpecialCharElement = // from .document //
+
+
+
+// Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// generate array from low to high
+
+// function arrayFromLowToHigh(low,high) {
+//   const array = []
+//   for (let i = low; i <= high; i++) {
+//     array.push(i)
+//   }
+//   return array
+// }
+
+// Write password to the #password input
+// function writePassword() {
+//   var characterCount = characterCountNumber.value
+//   var includeNumber = includeNumberElement.value
+//   var includeUppercase = includeUppercaseElement.value
+//   var includeLowercase = includeLowercaseElement.value
+
+//   var password = generatePassword(characterCount, includeNumber, includeUppercase, includeLowercase, includeSpecialChar) {
+
+
+
+
+
+
+//   }
+  
+
+  
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Generate the password
+
+
+
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+
+
